245dc4d15151512eb11a2ef8c253a36cb5f65e2d
Mohamed Magdy <moh.mag.ali@gmail.com>
2024-03-14 20:28:22 +0200

    setup db connection

--- src/startup/init.ts
+++ src/startup/init.ts
@@ -1,7 +1,17 @@
 import { Express } from "express";
+import typeORMConnect from "../database/typeorm";
 
-const appSetup = (app: Express) => {
+const appSetup = async (app: Express) => {
   const APP_PORT = process.env.APP_PORT || 4000;
+
+  try {
+    await typeORMConnect();
+    console.log("connected to database successfully");
+  } catch (error) {
+    console.log("unable to connect to database");
+    throw error;
+  }
+
   app.listen(APP_PORT, () => console.log(`Server started on port ${APP_PORT}`));
 };
 
