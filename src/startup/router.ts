6b634c69887977fd5f8d1b5f17fd2dc57cc7ba04
Mohamed Magdy <moh.mag.ali@gmail.com>
2024-03-14 20:39:19 +0200

    test db connection

--- src/startup/router.ts
+++ src/startup/router.ts
@@ -1,9 +1,17 @@
 import { Express, Request, Response } from "express";
+import { ProductService } from "../service/product.service";
 
 // routes
 const routerSetup = (app: Express) => {
   app.get("/", (req: Request, res: Response) => {
     res.status(200).send("Hello World!!");
+  });
+
+  app.get("/products", async (req: Request, res: Response) => {
+    const service = new ProductService();
+    const products = await service.getAll();
+
+    res.status(200).json(products);
   });
 };
 
