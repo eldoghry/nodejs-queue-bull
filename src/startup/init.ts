3dd51bfe2781b1071cd9153a0369a967717322e5
Mohamed Magdy <mohamed.eldoghry@contact.eg>
2024-03-17 15:55:33 +0200

    reset project and delete unnessecary files

--- src/startup/init.ts
+++ src/startup/init.ts
@@ -1,18 +1,10 @@
 import { Express } from "express";
-import typeORMConnect from "../database/typeorm";
 
 const appSetup = async (app: Express) => {
   const APP_PORT = process.env.APP_PORT || 4000;
-
-  try {
-    await typeORMConnect();
-    console.log("connected to database successfully");
-  } catch (error) {
-    console.log("unable to connect to database");
-    throw error;
-  }
-
-  app.listen(APP_PORT, () => console.log(`Server started on port ${APP_PORT}`));
+  app.listen(APP_PORT, () =>
+    console.log(`Server started http://localhost:${APP_PORT}`)
+  );
 };
 
 export default appSetup;
